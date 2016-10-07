94bd10c04dadf3b16300fea15fde7a8ec3d5b7fc
chendi1020 <chendi1020@gmail.com>
2016-10-07 12:58:47 -0400

    first commit casematrix function

--- cachematrix.R
+++ cachematrix.R
@@ -1,30 +1,31 @@
 ## This function creates a special "matrix" object that can cache its inverse
 
 
+
 makeCacheMatrix <- function(x = matrix()) {
-  m<-NULL
-  set<-function(y){
-    x<<-y
-    m<<-NULL
-  }
-  get<-function() x
-  setmatrix<-function(solve) m<<- solve
-  getmatrix<-function() m
-  list(set=set, get=get,
-       setmatrix=setmatrix,
-       getmatrix=getmatrix)
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setInverse <- function(solve) m <<- solve
+        getInverse <- function() m
+        list(set = set, get = get,
+             setInverse = setInverse,
+             getInverse = getInverse)
 }
 
 
 
 cacheSolve <- function(x, ...) {
-  m<-x$getmatrix()
-  if(!is.null(m)){
-    message("getting cached data")
-    return(m)
-  }
-  matrix<-x$get()
-  m<-solve(matrix, ...)
-  x$setmatrix(m)
-  m
+        m <- x$getInverse()
+        if(!is.null(m)) {
+                message("getting cached inverse matrix")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setInverse(m)
+        m
 }
